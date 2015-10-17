30871ee1edcc0426269d21bcc5ff8b60f4616771
Rohit Gurnani <rkgi10@gmail.com>
2015-10-18 01:05:03 +0530

    Initial Commit

---
+++ DesignDemo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DesignDemo1
+//
+//  Created by Rohit Gurnani on 18/10/15.
+//  Copyright © 2015 Rohit Gurnani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
